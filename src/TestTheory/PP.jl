ef71aee36d3de014b600961b869a3a98e6d14e44
Bian Kaiming <12231295@mail.sustech.edu.cn>
2024-06-04 16:48:39 +0800

    update the analysis of the order of PP

--- src/TestTheory/PP.jl
+++ src/TestTheory/PP.jl
@@ -2,19 +2,19 @@ module AnalysisPP
 export Lemma_1_2
 using Integrals
 
-function Lemma_1_2()
-	N = 20
+function Lemma_1_2(N, T)
+	# N = 200
 	function F(T)
 		f(u, p) = cos(u * pi / N)^T
 		domain = (1, N / 2 - 1)
 		prob = IntegralProblem(f, domain)
 		solve(prob, HCubatureJL(); reltol = 1e-3, abstol = 1e-3).u
 	end
-	T = 30
+	# T = 300
 
-	f(x) = cos(x * pi / N)^(T - 1) * sin(x * pi / N) * N / pi / T
+	g(x) = cos(x * pi / N)^(T - 1) * sin(x * pi / N) * N / pi / (T)
 
-	diff = (T - 1) / (T) * F(T - 2) + f(N / 2 - 1) - f(1) - F(T)
+	diff = (T - 1) / (T) * F(T - 2) + g(N / 2 - 1) - g(1) - F(T)
 	
 	return diff
 end
@@ -30,6 +30,6 @@ end # module AnalysisPP
 #%% Main
 using .AnalysisPP
 N = 20
-T = 30
-AnalysisPP.Lemma_1_2()
+T = 30                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
+AnalysisPP.Lemma_1_2(N,T)
 
